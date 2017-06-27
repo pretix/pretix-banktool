0c14caa19a8acd1cfaaa2718744bf74ae5c7f6ff
Raphael Michel <mail@raphaelmichel.de>
2017-06-27 17:08:22 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setup(
     name='pretix-banktool',
-    version='0.0.1',
+    version='0.1.0',
     description='Command-line client for pretix that allows to synchronize bank transaction statements to pretix',
     long_description=long_description,
     url='https://github.com/pretix/pretix-banktool',
