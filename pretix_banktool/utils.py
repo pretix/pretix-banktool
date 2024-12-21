5b5587aa1c4acf6e4b25c71818269ab73b059f1a
Yureka <yuka@yuka.dev>
2024-12-21 18:18:32 +0100

    ask_for_tan: show QR Code challenge

--- pretix_banktool/utils.py
+++ pretix_banktool/utils.py
@@ -1,7 +1,10 @@
+import io
 import click
 from fints.client import NeedTANResponse
 from fints.hhd.flicker import terminal_flicker_unix
 
+from PIL import Image
+from term_image.image import AutoImage
 
 def ask_for_tan(f, response):
     if not isinstance(response, NeedTANResponse):
@@ -13,5 +16,9 @@ def ask_for_tan(f, response):
             terminal_flicker_unix(response.challenge_hhduc)
         except KeyboardInterrupt:
             pass
+    if getattr(response, 'challenge_matrix', None):
+        img = Image.open(io.BytesIO(response.challenge_matrix[1]))
+        image = AutoImage(img)
+        image.draw()
     tan = input('Please enter TAN:')
     return f.send_tan(response, tan)
